507e0af677df7ad66ae6074f719928f81d7804a4
R1nge <r1ngegamedev@proton.me>
2026-02-06 23:28:39 +0000

    Modify snapshot_download to use local_dir_use_symlinks=False
    Updated snapshot_download call to disable local symlinks.

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,8 +19,7 @@ mkdir -p /data/input /data/output /data/voices
 MODEL_REPO="Misha24-10/F5-TTS_RUSSIAN"
 MODEL_CACHE="/root/.cache/huggingface/hub"
 # Скачиваем модель, если не скачана
-python3 -u -c "from huggingface_hub import snapshot_download; snapshot_download(repo_id='${MODEL_REPO}', cache_dir='${MODEL_CACHE}', allow_patterns=['F5-TTS_RUSSIAN/F5TTS_v1_Base/*','F5-TTS_RUSSIAN/F5TTS_v1_Base_v2/*'])"
-python3 -u -c "from huggingface_hub import snapshot_download; snapshot_download(repo_id='${MODEL_REPO}', cache_dir='${MODEL_CACHE}', allow_patterns=['F5TTS_v1_Base/*','F5TTS_v1_Base_v2/*'])"
+python3 -u -c "from huggingface_hub import snapshot_download; snapshot_download(repo_id='${MODEL_REPO}', cache_dir='${MODEL_CACHE}', allow_patterns=['F5TTS_v1_Base/*','F5TTS_v1_Base_v2/*'], local_dir_use_symlinks=False)"
 
 # run the API
 exec uvicorn app:app --host 0.0.0.0 --port ${PORT:-4123} --workers 1
