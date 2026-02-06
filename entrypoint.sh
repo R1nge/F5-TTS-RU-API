f20e79f8ef6732d2f04694d6af6cfa77196a4b25
R1nge <r1ngegamedev@proton.me>
2026-02-06 22:13:14 +0000

    Update model download patterns in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,7 @@ MODEL_REPO="Misha24-10/F5-TTS_RUSSIAN"
 MODEL_CACHE="/root/.cache/huggingface/hub"
 # Скачиваем модель, если не скачана
 python3 -u -c "from huggingface_hub import snapshot_download; snapshot_download(repo_id='${MODEL_REPO}', cache_dir='${MODEL_CACHE}', allow_patterns=['F5-TTS_RUSSIAN/F5TTS_v1_Base/*','F5-TTS_RUSSIAN/F5TTS_v1_Base_v2/*'])"
+python3 -u -c "from huggingface_hub import snapshot_download; snapshot_download(repo_id='${MODEL_REPO}', cache_dir='${MODEL_CACHE}', allow_patterns=['F5TTS_v1_Base/*','F5TTS_v1_Base_v2/*'])"
 
 # run the API
 exec uvicorn app:app --host 0.0.0.0 --port ${PORT:-4123} --workers 1
