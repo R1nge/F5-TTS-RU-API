170ae7853387b5417311c440d430e6b8b958f496
R1nge <r1ngegamedev@proton.me>
2026-02-07 09:44:46 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-#!/bin/bash 
+#!/bin/bash
 #Create new docker network
-sudo docker network create n8n_network
+docker network inspect n8n_network >/dev/null 2>&1 || docker network create n8n_network
 #Build the container
-sudo docker-compose up --build -d
+docker-compose up --build -d
